fb24868a5ed8028a82b768509f1149bb07e6e9e6
clocktower39 <clocktower39@aol.com>
2021-03-24 15:26:41 -0700

    moving to other device...

--- src/Components/AgentDrawer.js
+++ src/Components/AgentDrawer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {
+  Button,
   IconButton,
   Divider,
   Drawer,
@@ -13,12 +14,12 @@ import { makeStyles } from "@material-ui/core/styles";
 import { PersonSharp, Cancel } from "@material-ui/icons";
 
 const styles = makeStyles({
-  list: {
-    width: 250,
-  },
   fullList: {
     width: "auto",
   },
+  closeDrawerIcon: {
+    
+  }
 });
 
 class AgentDrawer extends Component {
@@ -29,23 +30,24 @@ class AgentDrawer extends Component {
         <React.Fragment key={"left"}>
           <Drawer anchor={"left"} open={this.props.isLeftOpen}>
             <div role="presentation">
-              <IconButton><Cancel onClick={this.props.toggleDrawer()} /></IconButton>
+              <Button>Show All</Button>
+              <IconButton onClick={this.props.toggleDrawer()} className={classes.closeDrawerIcon}><Cancel/></IconButton>
               <List className={classes.list}>
                 {this.props.agents.map((agent) => (
                   <ListItem
                     button
                     key={agent.lastName}
                   >
-                    <ListItemIcon>
-                      <PersonSharp />
-                    </ListItemIcon>
-                    <ListItemText primary={agent.load} />
+                    <ListItemIcon><PersonSharp /></ListItemIcon>
                     <ListItemText primary={`${agent.firstName} ${agent.lastName}`}/>
+                    {/* <ListItemText primary={'Programs'}>
                     <List>
                         {agent.programs.map(program => (
                           <ListItem>{program}</ListItem>
                         ))}
                     </List>
+                    </ListItemText>
+                    <ListItemText primary={agent.load} /> */}
                     <List>
 
                     </List>
