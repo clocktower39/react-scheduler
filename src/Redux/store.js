100347d7ccf26a38e23a8bd42c23ce71d6f6fd6a
clocktower39 <clocktower39@aol.com>
2021-05-31 19:48:28 -0700

    redux-thunk devtools

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer } from './reducer';
+import thunk from 'redux-thunk';
 
-export const store = createStore(
-    reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+export let store = createStore(
+    reducer,
+    composeWithDevTools(
+      applyMiddleware(thunk),
+    )
   );
